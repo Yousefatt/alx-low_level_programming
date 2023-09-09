9d2715e883eacae64389b7935d7b425899f67a72
Yousefatt <yousefattia852@gmail.com>
2023-09-09 00:40:23 -0400

    ls

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main-Entry point
+ *
+ *Description: print value of n satus; positive, negative or zero
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int v = 0;
+while (v < 10)
+{
+	putchar(v);
+	v++;
+}
+putchar('\n');
+return (0);
+}
