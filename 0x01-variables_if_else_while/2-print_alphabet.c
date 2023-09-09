eb7f6bac8ac2f81e4eb5b9b6e8f9ccfd4a7f0f09
Yousefatt <yousefattia852@gmail.com>
2023-09-08 20:38:57 -0400

    new

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+ *main-Entry point
+ *
+ *Description: print value of n satus; positive, negative or zero
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+char u;
+for (u = 'a' ; 'a' <= 'z' ; u++)
+{
+	putchar(u);
+}
+return (0);
+}
+
