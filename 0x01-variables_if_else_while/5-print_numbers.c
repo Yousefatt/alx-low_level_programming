d4fc171ad28d83a6ec551cb5f9d847edeef576a8
Yousefatt <yousefattia852@gmail.com>
2023-09-08 22:22:51 -0400

    n

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main-Entry point
+ *
+ *Description: print value of n satus; positive, negative or zero
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int f = 0;
+if (f <= 10)
+	printf("%d\n", f);
+return (0);
+}
