63cfe93880e95231ef1f59072852563966b90d3f
Yousefatt <yousefattia852@gmail.com>
2023-09-12 19:57:40 -0400

    edit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,15 +12,15 @@
 
 int print_sign(int n)
 {
-	if (n < 1)
-	{
-		_putchar('-');
-		return (-1);
-	}
-	else if (n > 0)
+	if (n > 1)
 	{
 		_putchar('+');
 		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
 	}
 	else
 	{
