abd6580881783b0f415a8350e8cf3113f8b23b9e
Yousefatt <yousefattia852@gmail.com>
2023-09-12 16:31:09 -0400

    some edit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char st[] = "_putchar";
+	int word;
+
+	for (word = 0; word < 8; ++word)
+		_putchar(st[word]);
+	_putchar('\n');
+
+	return (0);
+}
