06e741de99e35d16440372fc8ff8e4e5db558d25
Yousefatt <yousefattia852@gmail.com>
2023-09-12 16:57:51 -0400

    sec task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stido.h>
+#include "main.c"
+
+/**
+ * print_alphabet - utilizes on the _putchar function to print
+ *                 the alphabet a - z
+*/
+
+void print_alphabet(void)
+{
+int charac;
+for (charac = 'a'; charac <= 'z'; charac++)
+{
+	putchar(ch)
+}
+	putchar"\n"
+}
