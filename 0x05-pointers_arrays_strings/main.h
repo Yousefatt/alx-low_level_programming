2ad19a81a36597f7aaee9e2f31da30ef1d7079d8
Yousefatt <yousefattia852@gmail.com>
2023-09-19 20:47:36 -0400

    fifth task

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,9 +2,11 @@
 #define MAIN_H
 
 #include <stdio.h>
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+
+void print_rev(char *s);
 
 #endif /* MAIN_H */
