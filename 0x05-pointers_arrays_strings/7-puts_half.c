86072980fb35db99fc394f70beb937abd505c8d0
Yousefatt <yousefattia852@gmail.com>
2023-09-19 22:10:24 -0400

    dd

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ *puts_half - print sec half of string
+ *@str: char arra string
+ *Descrition: id odd numb of char,print (Lenth - 1) / 2
+ */
+
+void puts_half(char *str)
+{
+	int b;
+
+	for (b = 0; str[b] != '\0'; b++)
+		;
+
+	b++;
+	for (b /= 2; str[b] != '\0'; b++)
+	{
+		_putchar(str[b]);
+	}
+	_putchar('\n');
+}
