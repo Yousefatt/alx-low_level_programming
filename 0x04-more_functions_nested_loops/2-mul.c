ba6615203d3acaa1038767a9af12e3c0e5ff1079
Yousefatt <yousefattia852@gmail.com>
2023-09-25 17:57:12 -0400

    sec and third tasks

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * multi - multiplies two int
+ *
+ * @a: frist int
+ * @b: sec int
+ *
+ * Return: return result of this opp
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 1:9
+ *		only use _putchar twice
+ * Return: Always 0 (sussecc)
+ */
+
+void print_numbers(void)
+{
+	int n;
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar (n + '0');
+	}
+	_putchar ('\n');
+
+}
