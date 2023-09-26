fb0ee61a6f662d56314e87839dbde31184855edc
Yousefatt <yousefattia852@gmail.com>
2023-09-25 21:35:31 -0400

    edit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,12 +9,15 @@
 
 void print_most_numbers(void)
 {
-	int x = 0;
+	int x;
 
-	while (x != 2 && x != 4)
+	for (x = 0; x <= 9; x++)
 	{
-		_putchar (x + '0');
-		x++;
+		if (x == 2 || x == 4)
+		{
+			continue;
+		}
+		_putchar(x + 48);
 	}
-	_putchar ('\n');
+	_putchar('\n');
 }
