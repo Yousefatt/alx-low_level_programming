5cc54a70331385648dcc4105499f2597a28200a9
Yousefatt <yousefattia852@gmail.com>
2023-09-29 09:27:03 -0400

    edit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,10 +11,9 @@
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-        {
-                putchar ('\n');
-                return;
-        }
+	{
+		return;
+	}
 	_print_rev_recursion(s + 1);
-        putchar (*s);
+	putchar (*s);
 }
