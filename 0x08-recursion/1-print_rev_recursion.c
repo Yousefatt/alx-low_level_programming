e4127723f741c8719421a297c75f64824031a9c5
Yousefatt <yousefattia852@gmail.com>
2023-09-29 09:17:53 -0400

    task numb 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * void _print_rev_recursion - prints a string in reverse
+ *
+ * @s: pointer
+ *
+ * Return: Always 1 (Success)
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+        {
+                putchar ('\n');
+                return;
+        }
+	_print_rev_recursion(s + 1);
+        putchar (*s);
+}
