47391bf3b3c4f2384c81e095b7b8fcc4d3b8bc98
Yousefatt <yousefattia852@gmail.com>
2023-10-10 16:44:47 -0400

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+/**
+ * main - prints the name of the file it was compiled from,
+ *  followed by a new line
+ *
+ * Return: 0
+ */
+
+#include <stdio.h>
+
+int main ()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
