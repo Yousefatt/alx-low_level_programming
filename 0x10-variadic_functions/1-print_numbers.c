b890b99bafafa95211ccec68bf94ec226bc0a0ec
Yousefatt <yousefattia852@gmail.com>
2023-10-12 19:46:02 -0400

    ss

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int x;
 	va_list argu;
 
-	va_start(argu, n)
+	va_start(argu, n);
 	if (separator)
 		separator = "";
 	for (x = 0; x < n; x++)
